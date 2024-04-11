4e137f741f6a0b374052e5c2a5988b6a8aeb4b4c
Amartin02 <Tankfurnace641@gmail.com>
2024-04-11 13:13:49 -0600

    changes to category.js

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,29 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
+const { INTEGER } = require("sequelize");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      DataTypes: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    }, // define columns
+    category_name: {
+      DataTypes: STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
